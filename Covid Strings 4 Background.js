e3b4384ff2197491203242fab53c679be2335c1b
SGTIBALCOVIDSTRINGS <stephen.tiballi@daemen.edu>
2024-11-12 18:48:29 -0500

    Create Covid Strings 4 Background.js
    the background for covid strings 4 .

---
+++ Covid Strings 4 Background.js
@@ -0,0 +1,65 @@
+<doc type html>
+<script>
+filename=covid strings4 background .js
+java.awt graphics 
+img=document.createelement(covid)
+img.src=http://www.google.com/intl/en_com/images/logo_plain.png
+src=documentgetelementbyid(header)
+src.appendchild(img)
+<div id=header></div>
+audio=newaudio(audio_file mp3)
+audio.play 
+jbutton random button
+jbutton(set random color)
+private final j button option button=
+jbutton(change color)
+public fun graphics(){
+jpanel color panel=jpanel();
+add (panel,center )
+jpanel button panel=
+jpanel (newflowlayout(flowlayout.center)
+buttonpanel.add(randombutton);
+random.button addactionlistener(actionlistener(){
+override 
+publicactionperformed(actionevente){
+random r=newrandom();
+colorpanel.setbackground(rnextint(13),)(rnextint(13),)(rnextint(13),)(rnextint(13),)(rnextint(13),)(rnextint(13),)(rnextint(13),)(rnextint(13)))(rnextint(13)))(rnextint(13)))(rnextint(13)))(rnexint(13)))
+buttonpanel.add(optionbutton);
+optionbutton.add actionlistener(actionlistener(){
+override 
+publicactionPerformed(actionevente){
+color m=jcolorchooser.showdialog(funwithgraphics.this,)
+if(multi!=null)
+color panel set.background(multi)
+add(buttonpanel,borderlayout.south)
+public static main(string [] args){
+jframe f = covidstrings4()
+f.settitle(covid strings4)
+f.setcloseOperation(jframe.exit_on_close);
+f.setsize
+f.setvisible(true);
+<label id=lblscore>0</label>
+score=0
+if(mult == x*y)
+[non-concatenated]
+score+=ten
+else:
+score-=ten
+if(mult == x*y)
+[concatenated]
+score+=twenty
+else:
+score-=twenty
+lblscore=document.getelementbyid(lblscore)
+lbl.score.innerHTML=score
+private j button next button=jbutton(next)
+nextbutton add.actionlistener
+public actionperformed(action event e) 
+system.out print.in (next button clicked)
+frame.add(next button);
+frame set visible true
+object {a:one, b:two, c:three, d:four, e:five, f:six, g:seven, h:eight}
+keys object keys(team)
+entries objectentries(obj);
+value object=[team color]
+return filename
