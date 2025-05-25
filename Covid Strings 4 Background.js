73c0ae9b56864f76f5f1a3459cd89f3bce93e305
SGTIBALCOVIDSTRINGS <stephen.tiballi@daemen.edu>
2025-05-24 23:33:50 -0400

    Update Covid Strings 4 Background.js
    will edit changes later.

--- Covid Strings 4 Background.js
+++ Covid Strings 4 Background.js
@@ -1,5 +1,20 @@
-<doc type html>
-<script>
+doc type html
+html.lang.en
+script
+header=document create.element (header)
+header.style.backgroundcolor=#4caf50
+header.style.color=’white’
+header.style.padding=’10px’
+header.style.textalign=’center’
+header.style.text.fontsize=’24px’
+header.textcontent=’covid strings 4’ 
+document appendchild(header)
+heading1=#background overview
+heading2=#functions
+heading3=background code 
+<header.html>
+background overview
+background for covid strings4
 filename=covid strings4 background .js
 java.awt graphics 
 img=document.createelement(covid)
@@ -9,33 +24,39 @@ src.appendchild(img)
 <div id=header></div>
 audio=newaudio(audio_file mp3)
 audio.play 
-jbutton random button
-jbutton(set random color)
-private final j button option button=
-jbutton(change color)
-public fun graphics(){
-jpanel color panel=jpanel();
+random button
+button(set random color)
+option button
+button(change color)
+public fun graphics()
+color panel=jpanel();
 add (panel,center )
-jpanel button panel=
-jpanel (newflowlayout(flowlayout.center)
+button panel
+panel (newflowlayout(flowlayout.center)
 buttonpanel.add(randombutton);
-random.button addactionlistener(actionlistener(){
+random.button addactionlistener(actionlistener)
 override 
-publicactionperformed(actionevente){
+publicactionperformed(actionevente)
 random r=newrandom();
+<header.html>
+background functions
+functions used in covid strings4background
 colorpanel.setbackground(rnextint(13),)(rnextint(13),)(rnextint(13),)(rnextint(13),)(rnextint(13),)(rnextint(13),)(rnextint(13),)(rnextint(13)))(rnextint(13)))(rnextint(13)))(rnextint(13)))(rnexint(13)))
 buttonpanel.add(optionbutton);
-optionbutton.add actionlistener(actionlistener(){
+optionbutton.add actionlistener(actionlistener)
 override 
-publicactionPerformed(actionevente){
-color m=jcolorchooser.showdialog(funwithgraphics.this,)
+<header.html>
+background code 
+code to program game background
+publicactionperformed(actionevente)
+color m=jcolorchooser.showdialog(funwithgraphics.this)
 if(multi!=null)
 color panel set.background(multi)
 add(buttonpanel,borderlayout.south)
-public static main(string [] args){
+public static main(string [] args)
 jframe f = covidstrings4()
 f.settitle(covid strings4)
-f.setcloseOperation(jframe.exit_on_close);
+f.setcloseOperation(exit_on_close);
 f.setsize
 f.setvisible(true);
 <label id=lblscore>0</label>
@@ -51,8 +72,8 @@ score+=twenty
 else:
 score-=twenty
 lblscore=document.getelementbyid(lblscore)
-lbl.score.innerHTML=score
-private j button next button=jbutton(next)
+lbl.score.innerhtml=score
+next button=jbutton(next)
 nextbutton add.actionlistener
 public actionperformed(action event e) 
 system.out print.in (next button clicked)
